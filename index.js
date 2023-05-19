fef8be0893de3e7e4d144524bebb43df41fe2bb6
Mac <mac@Macs-MacBook-Air.local>
2023-05-19 16:57:16 +0600

    error handle changes

--- index.js
+++ index.js
@@ -3,12 +3,10 @@ const app = express();
 const cors =require('cors');
 const port = process.env.PORT || 5000 ;
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
-
-console.log(process.env.DB_PASS)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.00oqpy6.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -24,15 +22,28 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const carsCollection = client.db('carToys').collection('carToysTabs');
-
     app.get('/carToysTabs', async (req, res) => {
         const cursor = carsCollection.find();
         const result = await cursor.toArray();
         res.send(result);
     })
+
+    app.get('/carToysTabs/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) }
+
+        const options = {
+            // Include only the `title` and `imdb` fields in the returned document
+            projection: { type: 1, toys: 1, service_id: 1 },
+        };
+
+        const result = await carsCollection.findOne(query, options);
+        res.send(result);
+    })
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
